803581071361992fe0165215aea9e77d55cb61e4
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-09-27 11:45:28 +1000

    готовая самостоятельная работа

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace ISRPO_PR3_V5_Cherednichenko
         public MainWindow()
         {
             InitializeComponent();
+            X.Text = "";
+            Y.Text = "";
+            Z.Text = "";
+            Itog.Text = "Ст. гр. 402 ИСП Чередниченко Е.К.";
+        }
+
+        private void Schet_Click(object sender, RoutedEventArgs e)
+        {
+            Itog.Text += Environment.NewLine + "Практическая работа 3 Вариант 5";
+            //Считывание значения R
+            double x = double.Parse(X.Text);
+            //Вывод значения R в окно
+            Itog.Text += Environment.NewLine + "R = " + x.ToString();
+            //Считывание значения r
+            double y = double.Parse(Y.Text);
+            //Вывод значения r в окно
+            Itog.Text += Environment.NewLine + "r = " + y.ToString();
+            //Считывание знгачения l
+            double z = double.Parse(Z.Text);
+            //Вывод значения l в окно
+            Itog.Text += Environment.NewLine + "l = " + z.ToString();
+
+            //Вычисляем арифмитическое выражение
+            double a = Math.PI * (x + y) * z;
+            double b = Math.PI * x * x;
+            double c = Math.PI * y * y;
+            double s = a + b + c;
+            //Выводим результат в окно
+            Itog.Text += Environment.NewLine + "Результат S = " + s.ToString();
         }
     }
 }
