38d06a8b420385cebffef9eab649f035d89bc7ae
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-09-27 11:24:55 +1000

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ISRPO_PR3_V5_Cherednichenko
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
